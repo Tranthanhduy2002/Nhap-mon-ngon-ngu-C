6a65f01da2c6dcb6fac1977e9faa88c095aeb510
Tranthanhduy2002 <77660466+Tranthanhduy2002@users.noreply.github.com>
2021-01-21 16:57:17 +0700

    Create mang_05.c

---
+++ Bài tập 4/mang_05.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+  int a,b,c,d;
+  printf("nhap n");
+  scanf("%d, &n");
+
+  int a[n];
+  for(h=0;h < n; h++);
+  printf("a[%d] ": , h);
+  scanf("%d", &a[h]);
+}
+  printf(mang a:);
+  for(h = 0;h < n;h++){
+    printf("%d",a[h]);
+  }
+  printf("\n");
+  printf("Nhap c : ");
+  scanf("%d", &c);
+  for(h = 0;h < n;h++){
+    if(a[h] == c){
+      d=+1;
+      break;
+    }
+     }
+    if(d > 0){printf("Co ton tai");
+    }
+    else(printf("Khong ton tai"));
+
+
+
+
+
+  return 0;
+}
