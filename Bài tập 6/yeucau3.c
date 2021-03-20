0c1aa9379fd4a02ec1e2dd48f6af5a407eab7cf6
Tranthanhduy2002 <77660466+Tranthanhduy2002@users.noreply.github.com>
2021-03-20 14:58:54 +0700

    Create yeucau3.c

---
+++ Bài tập 6/yeucau3.c
@@ -0,0 +1,12 @@
+//khoi khai bao
+#include <stdio.h>
+int UCLN(int a, int b);
+int BCNN(int a, int b);
+//khoi ham main
+int main(void) {
+int a, b;
+  //nhap a, b tu ban phim
+printf("nhap a = ");
+scanf("%d", &a);
+printf("nhap b = ");
+scanf("%d", &b);
